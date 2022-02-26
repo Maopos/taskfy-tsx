78c6944411d34a6d02563551f0fed69c3946dc79
maopos <mauricioposadamurillo@gmail.com>
2022-02-26 18:04:37 -0500

    todo state & todos state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
+  // State
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const generateId = () => {
+    const random = Math.random().toString(36).substring(2);
+    const dateNow = Date.now().toString(36);
+    return random + dateNow;
+  };
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: generateId(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList />
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,32 @@
+import { useRef } from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  //
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
-      <input type="text" placeholder="Enter a text..." className="input__box" />
+    <form
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+      className="input"
+    >
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Enter a text..."
+        className="input__box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
       <button className="input_submit" type="submit">
         Save
       </button>
